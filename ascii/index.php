4d7706d6597b7a03c078f5eb26fc5d9f6504505e
dgstranz <dgstranz@users.noreply.github.com>
2013-10-17 18:04:57 +0200

    Minor fix

--- ascii/index.php
+++ ascii/index.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 echo '<table align="center" border="4"><tr align="center"><td colspan=16><h1>Tabla de caracteres ASCII</h1></td></tr>';
-for($cod=0;$cod<255;$cod++) {
+for($cod=0;$cod<=255;$cod++) {
 	if($cod%16==0) echo '<tr align="center">';
 	echo '<td><font face="Courier" size="-2">'
 		.$cod
