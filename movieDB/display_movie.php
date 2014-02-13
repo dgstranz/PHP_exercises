bb4ae02448bcaf8761a335d031853db4ac43d5fd
dgstranz <dgstranz@gmail.com>
2014-02-13 19:12:39 +0100

    movieDB - organized files

--- movieDB/display_movie.php
+++ movieDB/display_movie.php
@@ -4,20 +4,7 @@
 </head>
 <body>
 <?php
-// We'll use these functions to give the user more relevant data (strings) than raw IDs
-function get_type($movie_type) {
-	$select = 'SELECT movietype_label FROM movietype WHERE movietype_id = '.$movie_type;
-	$result = mysql_query($select);
-	$line = mysql_fetch_array($result, MYSQL_ASSOC);
-	return $line['movietype_label'];
-}
-
-function get_person($person) {
-	$select = 'SELECT people_fullname FROM people WHERE people_id = '.$person;
-	$result = mysql_query($select);
-	$line = mysql_fetch_array($result, MYSQL_ASSOC);
-	return $line['people_fullname'];
-}
+include 'functions/id_to_string.php';
 
 // Open connection, select DB and execute query
 $handle = mysql_connect('localhost', 'root', '') or die('Couldn\'t connect: ' . mysql_error());
