6a370708310412b63167cb934e05f161fe68601c
dgstranz <dgstranz@gmail.com>
2014-10-10 13:27:10 +0200

    Tweaked LCG pseudo-random generator

--- random2/index.php
+++ random2/index.php
@@ -1,21 +1,27 @@
 <html>
 <head>
+	<title>Pseudo-random number generator - LCG algorithm</title>
+	<meta charset="utf-8">
 </head>
 <body>
+	<h1>Pseudo-random number generator - LCG algorithm</h1>
+	<p>Note: There is an additional requirement for this exercise &dash; numbers may not be repeated.</p>
 <?php
-$iterations=100;
-$c=0;
+$iterations = 400;
+$c = 0;
+$seed = 1000000 * microtime();
+
+buildTable($iterations);
 
 function randomLCG($x,$pos) {
 	global $c;
 	return (219*$x[$pos]+$c)%32749;
-	return (2*$x[$pos]+$c)%100;
 }
 function noRepeat($x,$pos) {
 	global $c;
-	$i=1;
+	$i = 1;
 	while($i<$pos) {
-		if($x[$pos]!=$x[$i]) {
+		if($x[$pos] != $x[$i]) {
 			$i++;
 		}
 		else {
@@ -27,17 +33,18 @@ function noRepeat($x,$pos) {
 }
 
 function buildTable($iterations) {
+	global $seed;
 	$pos=0;
-	$x[0]=1;
-	$columns=10;
-	$rows=ceil($iterations/$columns);
+	$x[0]=$seed;
+	$columns=20;
+	$rows=ceil($iterations / $columns);
 	echo '<table border="4"><tr>';
 	for($i=1;$i<=$iterations;$i++) {
 		do {
 			$x[$pos+1]=randomLCG($x,$pos);
 		}
 		while(!noRepeat($x,$pos+1));
-		echo '<td>'.$x[$pos+1].'</td>';
+		echo '<td>' . $x[$pos + 1] . '</td>';
 		$pos++;
 		if($i%$columns==0) {
 			echo '</tr>';
@@ -46,8 +53,6 @@ function buildTable($iterations) {
 	}
 	echo '</table>';
 }
-
-buildTable($iterations);
 ?>
 </body>
 </html>
