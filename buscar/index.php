28e525d16a9537bf654c38a4254457f9b6c99434
dgstranz <dgstranz@gmail.com>
2014-10-13 12:35:24 +0200

    updated needle & haystack
    +possibility of searching for a given word of set of words (still not
    complete)

--- buscar/index.php
+++ buscar/index.php
@@ -8,23 +8,37 @@
 	$pajar;
 	$pos = array();
 
+	// Solamente se usan en caso de búsqueda por palabra
+	$palabras_aguja = array();
+	$palabras_pajar = array();
+
+	var_dump($_POST);
+
 	if (!isset($_POST['aguja']) || !isset($_POST['pajar'])) {
 		print_form();
 	} elseif (empty($_POST['aguja']) || empty($_POST['pajar'])) {
 		echo '<b>Error:</b> Alguno de los campos está vacío. Redireccionando en 5 segundos...';
-		header('refresh: 5, url=index.php');
+		header('refresh: 5, url=' . $_SERVER['PHP_SELF']);
 	} else {
 		$aguja = $_POST['aguja'];
 		$pajar = $_POST['pajar'];
 		print_form();
-		$pos = buscar_pos($aguja, $pajar);
+
+		if (isset($_POST['cadena'])) {
+			$pos = buscar_cadena($aguja, $pajar);
+		} elseif (isset($_POST['palabra'])) {
+			$palabras_aguja = obtener_palabras($aguja);
+			$palabras_pajar = obtener_palabras($pajar);
+			$pos = buscar_palabra($palabras_aguja, $palabras_pajar);
+		}
+
 		visualizar($pos);
 	}
 
 	function print_form() {
 		global $aguja;
 		global $pajar;
-		echo '<form action="index.php" method="post">
+		echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">
 				<table>
 					<tr>
 						<td>Texto:</td>
@@ -34,15 +48,32 @@
 						<td>Buscar:</td>
 						<td><input type="text" name="aguja" value="'.$aguja.'" /></td>
 					</tr>
-					<tr><td colspan="2"><input type="submit" value="Enviar" /><input type="reset" value="Borrar" /></td></tr>
+					<tr>
+						<td colspan="2">
+							<input type="submit" name="cadena" value="Buscar cadena" />
+							<input type="submit" name="palabra" value="Buscar palabra" />
+							<input type="reset" value="Borrar" />
+						</td>
+					</tr>
 				</table>
 			</form>';
 	}
 
-	function buscar_pos($aguja, $pajar) {
+	function obtener_palabras($cadena) {
+		$aux = trim($cadena);
+		$palabras;
+
+		$aux = preg_replace('/ +/', ' ', $aux);
+		$palabras = explode(' ', $aux);
+
+		return $palabras;
+	}
+
+	function buscar_cadena($aguja, $pajar) {
 		$pos_actual = 0;
 		$num_ocurrencias = 0;
 		$pos = array();
+		$valor;
 
 		while (($valor = stripos($pajar, $aguja, $pos_actual)) !== false) {
 			$pos[sizeof($pos)] = $valor;
@@ -52,8 +83,37 @@
 		return $pos;
 	}
 
+	function buscar_palabra($palabras_aguja, $palabras_pajar) {
+		$pos_actual = 0;
+		$num_ocurrencias = 0;
+		$pos = array();
+		$equal;
+
+		for ($j = 0; $j < sizeof($palabras_pajar) - (sizeof($palabras_aguja) - 1); $j++) {
+			$equal = true;
+
+			for ($i = 0; $equal && $i < sizeof($palabras_aguja); $i++) {
+				if ($palabras_aguja[$i] != $palabras_pajar[$j + $i]) {
+					$equal = false;
+				}
+			}
+
+			if ($equal) {
+				$pos[sizeof($pos)] = $j;
+			}
+		}
+
+		return $pos;
+	}
+
 	function visualizar($pos) {
 		global $aguja;
+		global $pajar;
+		$cont_pos = 0;
+		$cont_pajar = 0;
+
+		var_dump($pos);
+		var_dump($pajar);
 
 		switch ($pos) {
 			case !isset($pos):
